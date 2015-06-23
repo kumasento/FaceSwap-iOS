538777271da7c80d75f732060ee9aeb0ecec41cf
Vincent Zhao <vincentzhaorz@gmail.com>
2015-06-23 20:46:55 +0800

    Initial Commit

---
+++ FaceSwap-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceSwap-iOS
+//
+//  Created by Vincent Zhao on 6/23/15.
+//  Copyright (c) 2015 Vincent Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
