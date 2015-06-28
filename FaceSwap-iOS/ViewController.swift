ba16e38cadfd04755a0b195461b01cb7d27e68a7
Vincent Zhao <vincentzhaorz@gmail.com>
2015-06-28 21:15:34 +0800

    revised UI

--- FaceSwap-iOS/ViewController.swift
+++ FaceSwap-iOS/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
   var faceSwap: FaceSwap?
   
+  @IBOutlet weak var imageShadowView: UIView!
   @IBOutlet weak var imageView: UIImageView!
   @IBAction func selectImageFromAlbum(sender: AnyObject) {
     var imagePickerController = UIImagePickerController()
@@ -23,11 +24,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
   // This is a callback funtion, will activate the faceswap class from here
   func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
     imageView.image = image
-    imageView.layer.cornerRadius = imageView.frame.size.width/2
-    imageView.clipsToBounds = true
+    imageView.contentMode = UIViewContentMode.ScaleAspectFit
     
     // activate the faceswap module
-    faceSwap!.run(image)
+    // faceSwap!.run(image)
     
     picker.dismissViewControllerAnimated(true, completion: nil)
   }
@@ -38,6 +38,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     // initialize the new faceswap module
     faceSwap = FaceSwap()
+    
+    var cornerRadius = imageView.frame.size.width/2
+    
+    imageShadowView.layer.shadowOffset = CGSizeMake(0,0)
+    imageShadowView.layer.shadowOpacity = 1
+    imageShadowView.layer.shadowRadius = 5
+    imageShadowView.layer.shadowColor = UIColor.grayColor().CGColor
+    imageShadowView.layer.shadowPath = UIBezierPath(arcCenter: CGPoint(x: imageShadowView.frame.size.width/2, y: imageShadowView.frame.size.height/2), radius: cornerRadius, startAngle: 0, endAngle: 360, clockwise: true).CGPath
+    imageShadowView.layer.cornerRadius = cornerRadius
+    
+    imageView.layer.cornerRadius = cornerRadius
+    imageView.clipsToBounds = true
+    imageShadowView.addSubview(imageView)
   }
 
   override func didReceiveMemoryWarning() {
